e464ad1e4b734f44b66e1e6778c47d0815e2a740
Yassin charrouf errynda <ycha0707@g.educaand.es>
2024-05-04 12:03:33 +0200

    comit en la rama1

---
+++ src/gitentornos/ClaseRama1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitentornos;
+
+/**
+ *
+ * @author Yassin
+ */
+public class ClaseRama1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo¡¡");
+        int num1=1, num2=2;
+        System.out.println("la suma es: "+(num1+num2));
+    }
+}
