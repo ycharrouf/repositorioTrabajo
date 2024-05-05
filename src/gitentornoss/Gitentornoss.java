db91d5ef0eb32815356ec823cf05ef6815a9c164
Yassin charrouf errynda <ycha0707@g.educaand.es>
2024-05-05 15:51:14 +0200

    primer commit

---
+++ src/gitentornoss/Gitentornoss.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitentornoss;
+
+/**
+ *
+ * @author ----
+ */
+public class Gitentornoss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("vamos a hacer el push al repositorio el github");
+    }
+    
+}
